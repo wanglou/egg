489c36009c92ecb38c2749617abfc78ecfb1bdbb
wanglou <wangloul@163.com>
2021-06-09 10:02:12 +0800

    换ip

--- config/config.default.js
+++ config/config.default.js
@@ -17,7 +17,7 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: '39.105.229.131',
+      host: '124.70.88.47',
       // 端口号
       port: '3306',
       // 用户名
@@ -36,7 +36,7 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 9002,
+      port: 8082,
       hostname: '0.0.0.0',
     }
   };
